37988618bfbeef5c31b5dcee46c0043fad72b206
Iwan Morgan <ibmorgan0@gmail.com>
2017-09-14 12:42:58 +0100

    updated the "Add Entry" page to redirect to the index view.

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -55,8 +55,8 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
-
-                // TODO: Display the Entires list page.
+                
+                return RedirectToAction("Index");
             }
 
             return View(entry);
